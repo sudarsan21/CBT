a2d8e59dd84ae0aabd3cf271962c6959473926b5
Unknown <110298211+sudarsan21@users.noreply.github.com>
2022-12-09 12:47:20 +0530

    Add files via upload

---
+++ EX6/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PVS_BOX1 = new System.Windows.Forms.TextBox();
+            this.PVS_BOX2 = new System.Windows.Forms.TextBox();
+            this.PVS_BOX3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F);
+            this.label1.Location = new System.Drawing.Point(169, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Department:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F);
+            this.label2.Location = new System.Drawing.Point(151, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(180, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(169, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Register no:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(337, 317);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 35);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Store";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("News706 BT", 17.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(261, 40);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 27);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter the details";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // PVS_BOX1
+            // 
+            this.PVS_BOX1.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PVS_BOX1.Location = new System.Drawing.Point(337, 123);
+            this.PVS_BOX1.Name = "PVS_BOX1";
+            this.PVS_BOX1.Size = new System.Drawing.Size(151, 20);
+            this.PVS_BOX1.TabIndex = 8;
+            // 
+            // PVS_BOX2
+            // 
+            this.PVS_BOX2.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold);
+            this.PVS_BOX2.Location = new System.Drawing.Point(337, 181);
+            this.PVS_BOX2.Name = "PVS_BOX2";
+            this.PVS_BOX2.Size = new System.Drawing.Size(151, 20);
+            this.PVS_BOX2.TabIndex = 9;
+            // 
+            // PVS_BOX3
+            // 
+            this.PVS_BOX3.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold);
+            this.PVS_BOX3.Location = new System.Drawing.Point(334, 237);
+            this.PVS_BOX3.Name = "PVS_BOX3";
+            this.PVS_BOX3.Size = new System.Drawing.Size(154, 20);
+            this.PVS_BOX3.TabIndex = 10;
+            this.PVS_BOX3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PVS_BOX3);
+            this.Controls.Add(this.PVS_BOX2);
+            this.Controls.Add(this.PVS_BOX1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PVS_LAB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox PVS_BOX1;
+		private System.Windows.Forms.TextBox PVS_BOX2;
+		private System.Windows.Forms.TextBox PVS_BOX3;
+	}
+}
+
---
+++ EX6/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		public SqlConnection conn;
+		public SqlCommand comm;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			conn = new SqlConnection();
+			comm = new SqlCommand();
+			conn.ConnectionString = "Data Source=HPCSE-15\\SQLEXPRESS01;Initial Catalog=student;Integrated Security=true";
+			conn.Open();
+			comm.Connection = conn;
+			if (PVS_BOX1.Text == "" || PVS_BOX2.Text == "" || PVS_BOX3.Text == "")
+				MessageBox.Show("Please fill all the details");
+			else
+			{
+				comm.CommandText = "insert into stu values('"+PVS_BOX1.Text+"','"+PVS_BOX2.Text+"','"+PVS_BOX3.Text+"')";
+				comm.ExecuteNonQuery();
+				MessageBox.Show("1 record is stored successfully");
+			}
+			PVS_BOX1.Clear();
+			PVS_BOX2.Clear();
+			PVS_BOX3.Clear();
+
+		}
+
+		private void textBox3_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
