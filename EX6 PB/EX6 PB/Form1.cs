d60ddf5acf546e5096dd4dd03d805a226603265c
Unknown <110298211+sudarsan21@users.noreply.github.com>
2022-12-09 12:45:45 +0530

    Add files via upload

---
+++ EX6 PB/EX6 PB/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX6_PB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filepath = "B:\\CBT/EX6 PB/student.xml";
+            dataSet1.ReadXml(filepath);
+            dataGridView1.DataSource = dataSet1;
+            dataGridView1.DataMember = "stu";
+        }
+    }
+}
