a69356b6ee328ac1af2fca44d2c828850ce5b8fa
Unknown <110298211+sudarsan21@users.noreply.github.com>
2022-12-09 12:41:55 +0530

    Add files via upload

---
+++ EX3 PB/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection con;
+        public SqlCommand com;
+        public SqlDataReader rd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection();
+            com = new SqlCommand();
+            con.ConnectionString = "Data source=PVSHOME\\SQLEXPRESS;Initial Catalog=student;Integrated Security=true";
+            con.Open();
+            com.Connection = con;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            comboBox1.Visible = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            comboBox1.Visible = true;
+            com.CommandText = "select * from stu";
+            rd = com.ExecuteReader();
+            while (rd.Read())
+            {
+                comboBox1.Items.Add(rd.GetString(0));
+            }
+            rd.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            com.CommandText = "select * from stu where regno='" + comboBox1.SelectedItem + "'";
+            rd = com.ExecuteReader();
+            if (rd.Read())
+            {
+                textBox2.Text = rd.GetString(1);
+                textBox3.Text = rd.GetString(2);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "")
+                MessageBox.Show("please fill all the details");
+            else
+            {
+                com.CommandText = "insert('"+textBox1.Text+'","'+textBox2.Text+'","'+textBox3.Text+"')";
+                com.ExecuteNonQuery();
+                MessageBox.Show("record inserted");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+            com.CommandText = "delete from stu where regno='"+comboBox1.SelectedItem+"'";
+                rd = com.ExecuteReader();
+            comboBox1.Items.Remove(comboBox1.SelectedText);
+            textBox2.Clear();
+            textBox3.Clear();
+            MessageBox.Show("Record Deleted");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            com.CommandText = "update stu set sname='"+textBox2.Text+"',dept='"+textBox3.Text+"'whereregno'"+comboBox1.SelectedItem+"'";
+                com.ExecuteNonQuery();
+            MessageBox.Show("Record Updated");
+
+        }
+    }
+}
+
---
+++ EX3/EX3/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX3
+{
+    public partial class Form1 : Form
+    {
+        int b, n, d, f;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            n=int.Parse(textBox1.Text);
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "" + n;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("1");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("2");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("3");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("4");
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("5");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("6");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("7");
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("8");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("9");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("0");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            b=int.Parse(textBox1.Text);
+            op = "+";
+            textBox1.Clear();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            b = int.Parse(textBox1.Text);
+            op = "-";
+            textBox1.Clear();
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            b = int.Parse(textBox1.Text);
+            op = "*";
+            textBox1.Clear();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            b = int.Parse(textBox1.Text);
+            op = "/";
+            textBox1.Clear();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            d=int.Parse(textBox1.Text);
+            switch(op)
+            {
+                case "+":
+                    f = b + d;
+                    textBox1.Text = "" + f;
+                    break;
+                case "-":
+                    f = b - d;
+                    textBox1.Text = "" + f;
+                    break;
+                case "*":
+                    f = b * d;
+                    textBox1.Text = "" + f;
+                    break;
+                case "/":
+                    f = b / d;
+                    textBox1.Text = "" + f;
+                    break;
+            }
+        }
+
+        string op;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
